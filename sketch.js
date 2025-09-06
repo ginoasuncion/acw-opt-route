d8f0633e0361ddab2aefd606d72bc59599aa3f2d
ginoasuncion <ginoasuncion@gmail.com>
2025-09-06 20:21:35 +0700

    update max zoom level

--- sketch.js
+++ sketch.js
@@ -85,7 +85,7 @@ L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://www.carto.com/">CARTO</a>',
   subdomains: "abcd",
-  maxZoom: 15
+  maxZoom: 20
 }).addTo(map);
 
 // --- Marker styles ---
